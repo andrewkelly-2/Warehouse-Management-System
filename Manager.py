5c5f6d5d8f8b491e9dfa259dbdc15136b42d8017
Andrew Kelly <andrew.j.kelly-2@student.uts.edu.au>
2026-02-26 19:03:07 +1100

    Implement Manager.use() and __str__()

--- Manager.py
+++ Manager.py
@@ -9,8 +9,24 @@ class Manager(User):
         
     
     def use(self, org):
-        pass
+        print(f"Welcome, {self.first_name} {self.last_name}!")
+        print("1. View manager details")
+        print("2. view suppliers")
+        print("3. Exit")
+        menu_option_choice = input ("Enter choice: ")
+        if menu_option_choice == "1":
+            print(self)
+        elif menu_option_choice == "2":
+            print(self.suppliers)
+        elif menu_option_choice == "3":
+            print (f"Logging out {self.first_name} {self.last_name}...")
+            return
+        
+
     
     def __str__(self):
-        pass
+        suppliers = " "
+        for one_supplier in self.suppliers:
+            suppliers += f"{one_supplier}\n"
+        return f"{self.get_first_name()} {self.get_last_name()} {suppliers}"
 
